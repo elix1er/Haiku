94ade1de35d8f7443af1c213f0c0f23ada89f527
Anuban-corp <110306674+Anuban-corp@users.noreply.github.com>
2022-08-23 17:08:08 +0200

    Update event_manager.js
    Add documentation

--- packages/core/event_manager.js
+++ packages/core/event_manager.js
@@ -1,10 +1,19 @@
 let { EventSubscriber } = require('./event_subscriber');
 
+/**
+ * Manager to handle events.
+ */
 class EventManager {
   constructor() {
     this.subscribers = [];
   }
 
+  /**
+   * Wait an event fired.
+   * @param {any} emitter - The emitter.
+   * @param {string} type - The event type.
+   * @return {Promise} Promise containing event data.
+   */
   wait(emitter, type) {
     return new Promise(resolve => {
       this.subscribeOnce(emitter, type, this, (data) => {
@@ -13,6 +22,13 @@ class EventManager {
     });
   }
 
+  /**
+   * Add a subscriber.
+   * @param {any} emitter - The emitter.
+   * @param {string} type - The event type.
+   * @param {any} listener - The listener.
+   * @param {function} cb - The callback called when event is fired by emitter. Important note: The context is bind to listener at exec.
+   */
   subscribe(emitter, type, listener, cb) {
     if (!emitter) {
       throw new Error('EventManager::subscribe(): emitter is undefined !');
@@ -27,6 +43,13 @@ class EventManager {
     this.subscribers.push(new EventSubscriber(emitter, type, listener, false, cb));
   }
 
+  /**
+   * Add a subscriber. This subscriber is deleted after an event fired.
+   * @param {any} emitter - The emitter.
+   * @param {string} type - The event type.
+   * @param {any} listener - The listener.
+   * @param {function} cb - The callback called when event is fired by emitter. Important note: The context is bind to listener at exec.
+   */
   subscribeOnce(emitter, type, listener, cb) {
     if (!emitter) {
       throw new Error('EventManager::subscribe(): emitter is undefined !');
@@ -41,6 +64,12 @@ class EventManager {
     this.subscribers.push(new EventSubscriber(emitter, type, listener, true, cb));
   }
 
+  /**
+   * Remove a subscriber.
+   * @param {any} emitter - The emitter.
+   * @param {string} type - The event type.
+   * @param {any} listener - The listener.
+   */
   unsubscribe(emitter, type, listener) {
     for (let subscriber of this.subscribers) {
       if (subscriber.emitter == emitter && subscriber.type == type && subscriber.listener == listener) {
@@ -50,10 +79,20 @@ class EventManager {
     }
   }
 
+  /**
+   * Remove all subscribers.
+   */
   unsubscribeAll() {
     this.subscribers = [];
   }
 
+  /**
+   * Fire an event.
+   * @param {any} emitter - The emitter.
+   * @param {string} type - The event type.
+   * @param {any} data - Data payload.
+   * @return {Promise} - Return a promise resolved when all listeners have finished.
+   */
   async emit(emitter, type, data) {
     let promises = [];
 

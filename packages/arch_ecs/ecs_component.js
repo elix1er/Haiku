b5eea29a3e0ba324c568f7fc835837432bd97ccc
Anuban-corp <110306674+Anuban-corp@users.noreply.github.com>
2022-08-23 17:33:33 +0200

    Update ecs_component.js
    Add documentation

--- packages/arch_ecs/ecs_component.js
+++ packages/arch_ecs/ecs_component.js
@@ -1,4 +1,11 @@
+/**
+ * The ECSComponent is an abstract class for data-driven component.
+ */
 class ECSComponent {
+  /**
+   * Constructor
+   * @param {string} typename - A common way to identify a component.
+   */
   constructor(typename) {
     this.typename = typename;
   }

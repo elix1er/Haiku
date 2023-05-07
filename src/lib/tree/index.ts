b318f465ff207d9f8f88bec4eca43f44fe093548
jay19240 <110306674+jay19240@users.noreply.github.com>
2023-05-07 15:23:01 +0200

    update readme

--- src/lib/tree/index.ts
+++
@@ -1,6 +0,0 @@
-import BoundingBox from "./BoundingBox";
-import IPartitionMethod from "./IPartitionMethod";
-import Partition2D from "./Partition2D";
-import PartitionTreeNode from "./PartitionTreeNode";
-
-export {PartitionTreeNode,BoundingBox,IPartitionMethod,Partition2D};
